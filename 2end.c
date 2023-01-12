f72907e0aafe72f216ad0b09ab5bfc54a8e09b24
Sudharshank1430 <112713453+Sudharshank1430@users.noreply.github.com>
2023-01-12 20:28:06 +0530

    Add files via upload

---
+++ 2end.c
@@ -0,0 +1,23 @@
+#include<stdio.>
+
+int main()
+{
+    char ch;
+    FILE *fp;
+    
+    fp = fopen("char", "w");
+    if (fptr != NULL) 
+    {
+        printf("File created successfully!\n");
+    }
+    else 
+    {
+        printf("Failed to create the file.\n");
+        return 0;
+    }
+    fclose(fp)
+    return 0;
+}
+
+
+
